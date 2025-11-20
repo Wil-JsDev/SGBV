138c3b13becda67f5c69348249f6393035f1090e
Dayron <dayronbp06@gmail.com>
2025-11-20 12:40:16 -0400

    feat: add filter result

---
+++ src/Infrastructure/SGBV.Infrastructure.API/ServicesExtension/ServicesExtension.cs
@@ -0,0 +1,17 @@
+﻿using SGBV.Infrastructure.API.Filters;
+using SGBV.Infrastructure.API.Middlewares;
+
+namespace SGBV.Infrastructure.API.ServicesExtension;
+
+public static class ServicesExtension
+{
+    public static void AddFilters(this IMvcBuilder builder)
+    {
+        builder.AddMvcOptions(options => { options.Filters.Add<ResultFilter>(); });
+    }
+    
+    public static void UseGlobalException(this IApplicationBuilder builder)
+    {
+        builder.UseMiddleware<GlobalException>();
+    }
+}
