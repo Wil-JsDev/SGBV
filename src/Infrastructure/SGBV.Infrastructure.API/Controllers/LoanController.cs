28b9f462dc8031f426f60e67b4b348ddca10aab8
Wil-JsDev <wilmerdelacruz.dev@gmail.com>
2025-11-27 17:03:57 -0400

    feat: loan controller

---
+++ src/Infrastructure/SGBV.Infrastructure.API/Controllers/LoanController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using SGBV.Application.DTOs;
+using SGBV.Application.Interfaces.Services;
+using SGBV.Application.Utilities;
+
+namespace SGBV.Infrastructure.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class LoanController(ILoanService service) : ControllerBase
+{
+    [HttpPost]
+    public async Task<ResultT<LoanResponseDto>> CreateLoan([FromBody] LoanCreateDto loanDto) =>
+        await service.CreateLoanAsync(loanDto);
+
+    [HttpPut]
+    public async Task<ResultT<LoanReturnDto>> ReturnLoan([FromBody] LoanUpdateStatusDto loanDto) =>
+        await service.ReturnLoanAsync(loanDto);
+
+    [HttpDelete("{id:guid}")]
+    public async Task<ResultT<ResponseDto>> DeleteLoan([FromRoute] Guid id) =>
+        await service.DeleteLoanAsync(id);
+
+    [HttpGet("paged/{userId:guid}")]
+    public async Task<ResultT<PagedResult<LoanUserListDto>>> GetPagedLoanList([FromRoute] Guid userId,
+        [FromQuery] int pageNumber,
+        [FromQuery] int pageSize) =>
+        await service.GetPagedLoanListAsync(userId, pageNumber, pageSize);
+
+    [HttpGet("{loanId:guid}")]
+    public async Task<ResultT<LoanResponseDto>> GetById([FromRoute] Guid loanId) =>
+        await service.GetByIdAsync(loanId);
+}
