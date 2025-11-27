2ff2e223535606601a57d42c8413f1c65e6a1ea8
Wil-JsDev <wilmerdelacruz.dev@gmail.com>
2025-11-27 17:02:20 -0400

    feat: loan service DI

--- src/Application/SGBV.Application/DependencyInjection.cs
+++ src/Application/SGBV.Application/DependencyInjection.cs
@@ -10,13 +10,13 @@ public static class DependencyInjection
 {
     public static void AddApplicationLayer(this IServiceCollection services, IConfiguration configuration)
     {
-        
         #region Services
 
         services.AddScoped<ILoginService, LoginService>();
         services.AddScoped<IRegistrationService, RegistrationService>();
         services.AddScoped<IResourceService, ResourceService>();
         services.AddScoped<IUserService, UserService>();
+        services.AddScoped<ILoanService, LoanService>();
 
         #endregion
     }
